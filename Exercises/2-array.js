bf2be75f1b41ad05ad102e55ab36be80ad1a91ea
Hrystyna <yaskulskahrystyna@gmail.com>
2021-12-28 22:41:36 +0200

    Lab 5

--- Exercises/2-array.js
+++ Exercises/2-array.js
@@ -1,5 +1,11 @@
 'use strict';
 
-const array = () => null;
+const array = () => {
+  const arr = [];
+  const element = (i) => arr[i];
+  element.push = (value) => arr.push(value);
+  element.pop = () => arr.pop();
+  return element;
+};
 
 module.exports = { array };
